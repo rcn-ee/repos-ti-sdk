0954fe2990077be549daf076aa5d9f7adb2954c1
Robert Nelson <robertcnelson@gmail.com>
2020-03-23 11:49:29 -0500

    ti-tidl (01.03.00-bb.org-0.0-0rcnee0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 10-ti-tidl-9-debian/version.sh
+++ 10-ti-tidl-9-debian/version.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
-#https://github.com/rcn-ee/tidl-api/tree/v01.02.02-bb.org
+#https://github.com/rcn-ee/tidl-api/tree/v01.03.00-bb.org
 
 package_name="ti-tidl"
 debian_pkg_name="${package_name}"
-package_version="01.02.02-bb.org-0.2"
+package_version="01.03.00-bb.org-0.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/tidl-api.git"
-git_sha="b7bc34215efeeb8b2a16bb6e2c7385875aeb539f"
+git_sha="a01bde1a95f7ba820dc97ed949c729b9c1cf1f10"
 reprepro_dir="o/${package_name}"
 dl_path=""
 
-debian_version="${package_version}-0rcnee4"
+debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20200322"
+stretch_version="~stretch+20200323"

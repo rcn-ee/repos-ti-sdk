87434b39639a3a0e8099207dcf59bdd3d41ea073
Robert Nelson <robertcnelson@gmail.com>
2020-03-24 09:40:17 -0500

    ti-opencl (01.02.00.02-git20191223.1)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 09-ti-opencl-9-debian/version.sh
+++ 09-ti-opencl-9-debian/version.sh
@@ -5,12 +5,12 @@
 
 package_name="ti-opencl"
 debian_pkg_name="${package_name}"
-package_version="01.01.19.02-git20190930.1"
+package_version="01.02.00.02-git20191223.1"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="https://github.com/rcn-ee/ti-opencl.git"
-git_sha="53b3886e93aa89b3a2678e4fc24f0e5df4d1d94c"
+git_sha="bce78a3c602cb0db7948d137773c4d864894318b"
 reprepro_dir="c/${package_name}"
 dl_path=""
 
@@ -18,4 +18,4 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20200323"
+stretch_version="~stretch+20200324"

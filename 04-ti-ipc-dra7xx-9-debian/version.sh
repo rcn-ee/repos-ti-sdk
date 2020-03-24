0d1a26fda5d1a6285077601c0c62e2a8d1557103
Robert Nelson <robertcnelson@gmail.com>
2020-03-24 08:31:34 -0500

    ti-ipc-dra7xx (3.50.04.08-git20191028.0)
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 04-ti-ipc-dra7xx-9-debian/version.sh
+++ 04-ti-ipc-dra7xx-9-debian/version.sh
@@ -5,13 +5,13 @@
 
 package_name="ti-ipc-dra7xx"
 debian_pkg_name="${package_name}"
-package_version="3.50.04.07-git20190830.0"
+package_version="3.50.04.08-git20191028.0"
 package_source="${package_name}_${package_version}.orig.tar.xz"
 src_dir="${package_name}_${package_version}"
 
 git_repo="git://git.ti.com/ipc/ipcdev.git"
-#https://git.ti.com/gitweb?p=ipc/ipcdev.git;a=shortlog;h=129b6f44c50cbe760a5c1481b34b323b7a42ead5
-git_sha="129b6f44c50cbe760a5c1481b34b323b7a42ead5"
+#https://git.ti.com/gitweb?p=ipc/ipcdev.git;a=shortlog;h=1d53b856338b86d1979fe09345c0b382913a848a
+git_sha="1d53b856338b86d1979fe09345c0b382913a848a"
 reprepro_dir="t/${package_name}"
 dl_path=""
 
@@ -19,4 +19,4 @@ debian_version="${package_version}-0rcnee0"
 debian_patch=""
 debian_diff=""
 
-stretch_version="~stretch+20200323"
+stretch_version="~stretch+20200324"
